4c64185ea797b09ba0a7df8b1b74e3b35affe6f5
bitcoin <bitcoin@honeypot>
2023-06-11 12:56:00 +0000

    Rework getroute excludes

--- lightning_pay.py
+++ lightning_pay.py
@@ -12,6 +12,7 @@ from channel_exclude import *
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("bolt11_or_destination_id")
 parser.add_argument("-use_sendpay", default="no", type=str, nargs="?")
+parser.add_argument("-scid_out", type=str, nargs="?")
 parser.add_argument("amount_in_milli_satoshi", default=None, type=int, nargs="?")
 parser.add_argument("payment_hash", nargs="?")
 parser.add_argument("min_final_cltv_expiry", nargs="?")
@@ -32,6 +33,8 @@ if use_bolt11:
     print(bolt11)
     #print("Bolt11 decoded:\n%s" % json.dumps(bolt11, indent=4))
     id_ = bolt11["payee"]
+
+
     payment_hash = bolt11["payment_hash"]
     print("id = " + id_ + "  payment_hash = " + payment_hash)
     if "msatoshi" in bolt11:
@@ -48,10 +51,35 @@ if use_bolt11:
         payment_secret = bolt11["payment_secret"]
         min_cltv_expiry = bolt11["min_final_cltv_expiry"]
         
-        excludes = getExcludes(ld, id_)
+        print(id_)
+        out_chan = id_
+#        if args.scid_out is not None:
+#            print(args.scid_out)
+#            output = ld.listchannels(args.scid_out)
+#            chan = output["channels"]
+#            print(chan[0])
+#            print(chan[1])
+#            out_chan = chan[1]["destination"]
+#            route = ld.getroute(id_, amount, fromid=out_chan, riskfactor=100, cltv=min_cltv_expiry)
+#            delay = route["route"][0]["delay"]
+#            fee = route["route"][0]["msatoshi"] - amount
+#            print(delay)
+#            print(output)
+
+#        print(out_chan)
+
+        excludes = getExcludes(ld, out_chan)
+#        output = ld.getroute(out_chan, amount, maxhops=1, riskfactor=100, cltv=min_cltv_expiry, exclude=excludes)
+#        leg = output["route"][0]
+#        leg["delay"] = leg["delay"] + delay
+#        leg["msatoshi"] = leg["msatoshi"] + fee + 2000
+#        leg["amount_msat"] = leg["amount_msat"] + fee + 2000
+#        print(leg)
+#        route["route"].insert(0, leg)
 
         route = ld.getroute(id_, amount, maxhops=1, riskfactor=100, cltv=min_cltv_expiry, exclude=excludes)
         print(route)
+#        exit()
         fee = route["route"][0]["msatoshi"] - amount
 
         reply = input("Paying fee %s on amount %s (%.3f%%). Send [Y/n]? " % (fee, amount, fee / amount * 100.0))
